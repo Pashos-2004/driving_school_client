bec1ace983262e2f4f676d90b775ebb48f3e9b93
Pashos-2004 <Pashos-2004@yandex.ru>
2025-05-04 01:14:52 +0300

    ignore

--- src/driving_school/main.java
+++ src/driving_school/main.java
@@ -1,19 +1,36 @@
 package driving_school;
 
-import io.dotenv.Dotenv;
 import javax.swing.*;
-
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 public class main {
+	
 	
 	
 	
 	public static void main (String arg[]) {
 		
+	
+		
 		JFrame JF = new JFrame() {};
 		JF.setVisible(true);
 		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JF.setSize(720, 480);
+		JF.setLocation(650,250);
+		JF.setTitle("Driving school clinet");
 		
+		
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File("src/pictures/icon.png"));
+			JF.setIconImage(img);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		
+		}
 	}
 	
 	
