31e1bdad9e12a4eb204dd5273f76bb72ec3c4019
Pashos-2004 <Pashos-2004@yandex.ru>
2025-05-03 22:44:02 +0300

    first

---
+++ src/driving_school/main.java
@@ -0,0 +1,19 @@
+package driving_school;
+
+import javax.swing.*;
+
+public class main {
+	
+	
+	public static void main (String arg[]) {
+		
+		JFrame JF = new JFrame() {};
+		JF.setVisible(true);
+		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JF.setSize(720, 480);
+		
+		
+	}
+	
+	
+}
