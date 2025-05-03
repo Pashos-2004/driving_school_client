5db522f9b3c943e7f6bf60ceda9a075ca1a15b2f
Pashos-2004 <Pashos-2004@yandex.ru>
2025-05-04 01:47:04 +0300

    Добавлено главное окно с автоматическим определением своего размера

---
+++ src/driving_school/commonData.java
@@ -0,0 +1,11 @@
+package driving_school;
+
+public class commonData {
+	public static int screenWidth = 160;
+	public static int screenHeigh = 90;
+	public static int appWidth = 160;
+	public static int appHeight = 90;
+	
+	
+	
+}
--- src/driving_school/main.java
+++ src/driving_school/main.java
@@ -1,10 +1,16 @@
 package driving_school;
 
 import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+
+
+
 public class main {
 	
 	
@@ -13,23 +19,33 @@ public class main {
 	public static void main (String arg[]) {
 		
 	
-		
+		BufferedImage appIcon;
 		JFrame JF = new JFrame() {};
+		Toolkit toolKit = Toolkit.getDefaultToolkit();
+		
+		commonData.screenHeigh = toolKit.getScreenSize().height;
+		commonData.screenWidth = toolKit.getScreenSize().width;
+		commonData.appWidth = commonData.screenWidth/2;
+		commonData.appHeight = commonData.screenHeigh/2;
+		
+		//System.out.println("Ширина экрана "+commonData.screenWidth);	
+		//System.out.println("Высота экрана "+commonData.screenHeigh);	
+		
 		JF.setVisible(true);
 		JF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JF.setSize(720, 480);
-		JF.setLocation(650,250);
+		JF.setBounds(commonData.screenWidth/2-commonData.appWidth/2, commonData.screenHeigh/2-commonData.appHeight/2, commonData.appWidth, commonData.appHeight);
 		JF.setTitle("Driving school clinet");
 		
 		
-		BufferedImage img;
-		try {
-			img = ImageIO.read(new File("src/pictures/icon.png"));
-			JF.setIconImage(img);
-		} catch (IOException e) {
-			
-			e.printStackTrace();
 		
+		try {
+			appIcon = ImageIO.read(new File("src/pictures/icon.png"));
+			JF.setIconImage(appIcon);
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+			//System.out.println(e.getLocalizedMessage());
+			
 		}
 	}
 	
